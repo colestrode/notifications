7fc8f8053dc210b28b0ca45713f0dbe3b76771ed
Cole Furfaro-Strode <colestrode@gmail.com>
2016-09-29 15:45:36 -0400

    cleans up filters -> transforms renaming

--- src/transforms/index.js
+++ src/transforms/index.js
@@ -2,25 +2,25 @@
 
 const q = require('q')
 const _ = require('lodash')
-const filters = require('require-all')({
+const transforms = require('require-all')({
   dirname: __dirname,
   recursive: true
 })
 
-delete filters.index
+delete transforms.index
 
 /**
- * Filters can modify or remove any recipient in the array. They should handle empty arrays.
+ * Transforms can modify or remove any recipient in the array. They should handle empty arrays.
  * Each filter should be exported as a function that accepts an array of recipients and returns a promise that resolves
  * with the new modified array of recipients. This could be the array that was passed in or a new array.
- * Most filters will be synchronous, but they should return a promise to support possible asychronous handling in the future.
+ * Most transforms will be synchronous, but they should return a promise to support possible asychronous handling in the future.
  *
  * @param recipients
  * @returns {*}
  */
 module.exports = function (recipients) {
-  // processes filters in series
-  return _.reduce(filters, (promise, filter) => {
+  // processes transforms in series
+  return _.reduce(transforms, (promise, filter) => {
     return promise.then(filter)
   }, q(recipients))
 }
